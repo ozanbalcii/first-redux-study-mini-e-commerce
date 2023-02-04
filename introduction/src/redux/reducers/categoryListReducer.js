c979f61c2f75d91fd46ead08832efc7034c0463c
ozan balcı <102185183+ozanbalcii@users.noreply.github.com>
2023-02-04 13:21:14 +0300

    Update categoryListReducer.js

--- introduction/src/redux/reducers/categoryListReducer.js
+++ introduction/src/redux/reducers/categoryListReducer.js
@@ -1,7 +1,6 @@
 import * as actionTypes from "../actions/actionTypes";
 import initialState from "./initialState";
 
-
 export default function changeCategoryReducer(state = initialState.categories, action){
   switch (action.type) {
     case actionTypes.GET_CATEGORIES_SUCCESS:
