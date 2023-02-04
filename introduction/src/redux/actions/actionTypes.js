a6d384462b04895269ba3c30743d171f60ae0782
ozan <ozanbalcieem@gmail.com>
2023-02-04 13:02:37 +0300

    Create actionTypes.js

---
+++ introduction/src/redux/actions/actionTypes.js
@@ -0,0 +1,11 @@
+export const CHANGE_CATEGORY = "CHANGE_CATEGORY";
+export const GET_CATEGORIES_SUCCESS = "GET_CATEGORIES_SUCCESS"; //! veri tabanına post, delete vs yapacaksak, bunu eklememiz gerekir.
+
+export const GET_PRODUCTS_SUCCESS = "GET_PRODUCTS_SUCCESS"; //* ürünlerin listelenmesi için(products to list)
+
+export const CREATE_PRODUCT_SUCCESS = "CREATE_PRODUCT_SUCCESS"; //* products to save in api or database
+export const UPDATE_PRODUCT_SUCCESS = "UPDATE_PRODUCT_SUCCESS"; //* products to update in api or database
+
+
+export const ADD_TO_CART = "ADD_TO_CART";
+export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
