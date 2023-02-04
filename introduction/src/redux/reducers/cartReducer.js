b2320be61fdeb862754d0f7c10e1570ecec2dd69
ozan balcı <102185183+ozanbalcii@users.noreply.github.com>
2023-02-04 13:21:06 +0300

    Update cartReducer.js

--- introduction/src/redux/reducers/cartReducer.js
+++ introduction/src/redux/reducers/cartReducer.js
@@ -6,7 +6,7 @@ export default function cartReducer(state = initialState.cart, action) {
     case actionTypes.ADD_TO_CART:
       var addedItem = state.find(
         (c) => c.product.id === action.payload.product.id
-      ); //!eklenen ürün daha önce sepette var mı kontrolü
+      );
       if (addedItem) {
         var newState = state.map((cartItem) => {
           if (cartItem.product.id === action.payload.product.id) {
@@ -18,7 +18,7 @@ export default function cartReducer(state = initialState.cart, action) {
         });
         return newState;
       } else {
-        return [...state, { ...action.payload }]; //! array'in kopyasını alıp eleman ekle---> state'in kopyasını al ve action ile gelen payload'a ekle
+        return [...state, { ...action.payload }];
       }
       case actionTypes.REMOVE_FROM_CART:
         const newState2 = state.filter(cartItem=> cartItem.product.id!==action.payload.id) //! when ids are not equal, filter
