755745aa2287526a773e2f57ce42f2144544a8d4
ozan balcı <102185183+ozanbalcii@users.noreply.github.com>
2023-02-04 13:16:39 +0300

    Update TextInput.js

--- introduction/src/components/toolbox/TextInput.js
+++ introduction/src/components/toolbox/TextInput.js
@@ -1,14 +1,11 @@
 import React from "react";
 
-//! react hooks'ta fonksiyonel componentlerde props'ları parametre olarak alıyoruz. yani buraya yazılan parametreler props'lardır.
 const TextInput = ({ name, label, onChange, placeHolder, value, error }) => {
   let wrapperClass = "form-group";
   if (error && error.length > 0) {
     wrapperClass += " has-error";
   }
-
   return (
-    /* burada boostrap ile from yapısı kuruyoruz: */
     <div className={wrapperClass}>
       <label htmlFor={name}>{label}</label>
       <div className="field">
@@ -25,5 +22,4 @@ const TextInput = ({ name, label, onChange, placeHolder, value, error }) => {
     </div>
   );
 };
-
 export default TextInput;
