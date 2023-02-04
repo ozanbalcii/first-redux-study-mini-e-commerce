94a6a568276b1d008c3eae1c30d7522c0f2915b3
ozan balcı <102185183+ozanbalcii@users.noreply.github.com>
2023-02-04 13:21:51 +0300

    Update initialState.js

--- introduction/src/redux/reducers/initialState.js
+++ introduction/src/redux/reducers/initialState.js
@@ -1,10 +1,7 @@
-//! initial(baş, baştaki): başlangıç state'lerini tutacak
-
 export default {
     currentCategory: {},
     categories : [],
     products: [],
     cart: [],
-    saveProduct: {},
-    
+    saveProduct: {}, 
 }
