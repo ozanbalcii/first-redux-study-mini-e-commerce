94761eb497605f5b2377a7a255308e213f3d79ce
ozan balcı <102185183+ozanbalcii@users.noreply.github.com>
2023-02-04 13:17:44 +0300

    Update actionTypes.js

--- introduction/src/redux/actions/actionTypes.js
+++ introduction/src/redux/actions/actionTypes.js
@@ -1,10 +1,10 @@
 export const CHANGE_CATEGORY = "CHANGE_CATEGORY";
-export const GET_CATEGORIES_SUCCESS = "GET_CATEGORIES_SUCCESS"; //! veri tabanına post, delete vs yapacaksak, bunu eklememiz gerekir.
+export const GET_CATEGORIES_SUCCESS = "GET_CATEGORIES_SUCCESS"; 
 
-export const GET_PRODUCTS_SUCCESS = "GET_PRODUCTS_SUCCESS"; //* ürünlerin listelenmesi için(products to list)
+export const GET_PRODUCTS_SUCCESS = "GET_PRODUCTS_SUCCESS"; //!(products to list)
 
-export const CREATE_PRODUCT_SUCCESS = "CREATE_PRODUCT_SUCCESS"; //* products to save in api or database
-export const UPDATE_PRODUCT_SUCCESS = "UPDATE_PRODUCT_SUCCESS"; //* products to update in api or database
+export const CREATE_PRODUCT_SUCCESS = "CREATE_PRODUCT_SUCCESS"; //! products to save in api or database
+export const UPDATE_PRODUCT_SUCCESS = "UPDATE_PRODUCT_SUCCESS"; //! products to update in api or database
 
 
 export const ADD_TO_CART = "ADD_TO_CART";
