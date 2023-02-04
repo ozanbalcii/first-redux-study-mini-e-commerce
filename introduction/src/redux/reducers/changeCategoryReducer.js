bab19ff001116b8f0008d97191d43ddb538eb238
ozan balcı <102185183+ozanbalcii@users.noreply.github.com>
2023-02-04 13:21:26 +0300

    Update changeCategoryReducer.js

--- introduction/src/redux/reducers/changeCategoryReducer.js
+++ introduction/src/redux/reducers/changeCategoryReducer.js
@@ -1,12 +1,11 @@
 import * as actionTypes from "../actions/actionTypes";
 import initialState from "./initialState";
 
-
 export default function changeCategoryReducer(state = initialState.currentCategory, action){
   switch (action.type) {
     case actionTypes.CHANGE_CATEGORY:
       return action.payload;
-    default:  //! eğer actions yoksa default olarak state'i return et.
+    default:  
       return state;
   }
 }
