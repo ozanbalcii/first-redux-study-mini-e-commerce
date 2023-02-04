06768a602be1cfc93a1349b0982424c34f255d04
ozan balcı <102185183+ozanbalcii@users.noreply.github.com>
2023-02-04 13:16:23 +0300

    Update SelecetInput.js

--- introduction/src/components/toolbox/SelecetInput.js
+++ introduction/src/components/toolbox/SelecetInput.js
@@ -1,9 +1,6 @@
 import React from "react";
 
-
-
 const SelectInput = ({name, label, onChange, defaultOption, value, error, options}) => {
-
     return (
         <div className="form-group">
         <label htmlFor={name}>{label}</label>
