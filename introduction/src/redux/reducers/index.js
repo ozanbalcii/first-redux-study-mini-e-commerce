8e7c6e2943cb4efca2bbe8f1bc433f037c0f8d2c
ozan balcı <102185183+ozanbalcii@users.noreply.github.com>
2023-02-04 13:21:39 +0300

    Update index.js

--- introduction/src/redux/reducers/index.js
+++ introduction/src/redux/reducers/index.js
@@ -5,15 +5,12 @@ import productListReducer from "./productListReducer";
 import cartReducer from "./cartReducer";
 import saveProductReducer from "./saveProductReducer";
 
-
 const rootReducer = combineReducers({
     changeCategoryReducer,
     categoryListReducer, 
     productListReducer,
     cartReducer,
     saveProductReducer, 
-
 });
 
-
-export default( rootReducer);
+export default(rootReducer);
